2b87594b77040453201bb5e7a5dcedfd147944b7
Edward Choi (RIT Student) <ec2244@rit.edu>
2024-10-03 15:12:51 -0400

    added pe08 start

---
+++ PEs/PE-08 Audio Visualizer/web-audio-hw/src/main.js
@@ -0,0 +1,36 @@
+/*
+	main.js is primarily responsible for hooking up the UI to the rest of the application 
+	and setting up the main event loop
+*/
+
+// We will write the functions in this file in the traditional ES5 way
+// In this instance, we feel the code is more readable if written this way
+// If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
+
+import * as utils from './utils.js';
+
+// 1 - here we are faking an enumeration
+const DEFAULTS = Object.freeze({
+	sound1  :  "media/New Adventure Theme.mp3"
+});
+
+function init(){
+	console.log("init called");
+	console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
+	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+	setupUI(canvasElement);
+}
+
+function setupUI(canvasElement){
+  // A - hookup fullscreen button
+  const fsButton = document.querySelector("#fsButton");
+	
+  // add .onclick event to button
+  fsButton.onclick = e => {
+    console.log("goFullscreen() called");
+    utils.goFullscreen(canvasElement);
+  };
+	
+} // end setupUI
+
+export {init};
